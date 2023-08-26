6c4320ebbe1c3d98941f529736605d09f8a2ca01
Arad Firouzkouhi <aradfir@gmail.com>
2023-08-26 11:59:30 +0330

    adjecancy matrix definition

---
+++ bfs.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+//adjacency_matrix
+vector<vector<int>> adj_matrix{
+{0,1,1,0,1,0,0},
+{1,0,1,0,1,0,0},
+{1,1,0,1,0,0,0},
+{0,0,1,0,0,1,0},
+{1,1,0,0,0,1,0},
+{0,0,0,1,1,0,1},
+{0,0,0,0,0,1,0},
+};
+
+int main(){
+    return 0;
+}
